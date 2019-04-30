f47f80e3e1cb7b0388a553a7d82a0be9a774cbae
Sagar Bhosale <sbbho@nets.eu>
2019-04-30 19:56:30 +0530

    Swagger Changes

--- UnitTestDemo/src/main/java/com/unitTest/UnitTestDemo/controller/EmployeeRestController.java
+++ UnitTestDemo/src/main/java/com/unitTest/UnitTestDemo/controller/EmployeeRestController.java
@@ -2,6 +2,8 @@ package com.unitTest.UnitTestDemo.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.unitTest.UnitTestDemo.entity.Employee;
 import com.unitTest.UnitTestDemo.service.EmployeeService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -21,6 +26,7 @@ import lombok.extern.slf4j.Slf4j;
  */
 @RestController
 @Slf4j
+@Api(value = "Employee Management System", description = "Operations pertaining to employee in Employee Management System")
 public class EmployeeRestController {
 
 	@Autowired
@@ -30,24 +36,28 @@ public class EmployeeRestController {
 		this.employeeService = employeeService;
 	}
 
-	
-
+	@ApiOperation(value = "Get an employee by Id")
 	@GetMapping("/api/employees/{employeeId}")
-	public Employee getEmployee(@PathVariable(name = "employeeId") Long employeeId) {
+	public Employee getEmployee(
+			@ApiParam(value = "Employee id from which employee object will retrieve", required = true) @PathVariable(name = "employeeId") Long employeeId) {
 		return employeeService.getEmployee(employeeId);
 	}
 
+	@ApiOperation(value = "Add an employee")
 	@PostMapping("/api/employees")
-	public String saveEmployee(@RequestBody Employee employee) {
+	public String saveEmployee(
+			@ApiParam(value = "Employee object store in database table", required = true) @Valid @RequestBody Employee employee) {
 		employeeService.saveEmployee(employee);
 		log.debug("Employee Saved Successfully");
 		return employee.getName();
 	}
 
+	@ApiOperation(value = "Delete an employee")
 	@DeleteMapping("/api/employees/{employeeId}")
-	public int deleteEmployee(@PathVariable(name = "employeeId") Long employeeId) {
+	public int deleteEmployee(
+			@ApiParam(value = "Employee Id from which employee object will delete from database table", required = true) @PathVariable(name = "employeeId") Long employeeId) {
 		return employeeService.deleteEmployee(employeeId);
-		
+
 	}
 
 }
--- UnitTestDemo/src/main/java/com/unitTest/UnitTestDemo/entity/Employee.java
+++ UnitTestDemo/src/main/java/com/unitTest/UnitTestDemo/entity/Employee.java
@@ -7,6 +7,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,18 +22,20 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ApiModel(description = "All details about the Employee. ")
 public class Employee {
+	@ApiModelProperty(notes = "The database generated employee ID")
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
+	@ApiModelProperty(notes = "The database generated employee ID")
 	@Column(name = "EMPLOYEE_NAME")
 	private String name;
-
+	@ApiModelProperty(notes = "The database generated employee ID")
 	@Column(name = "EMPLOYEE_SALARY")
 	private Integer salary;
-
+	@ApiModelProperty(notes = "The database generated employee ID")
 	@Column(name = "DEPARTMENT")
 	private String department;
-	
+
 }
